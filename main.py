b01ad6e38c59740a06ca90ac278f7ce402b4134a
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-08-15 17:47:11 -0300

    M5.L1 - Actividad #5: "La función update(dt)"
    Nos familiarizamos con la función update()

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #pgzero
 
 """
-# M5.L1 - Actividad #4: "Personaje"
-# Objetivo: enseñar a los alumnos a crear Actores y mostrarlos por pantalla
+# M5.L1 - Actividad #5: "La función update(dt)"
+# Objetivo: familiarizarnos con la función update()
 """
 WIDTH = 600 # Ancho de la ventana (en px)
 HEIGHT = 300 # Alto de la ventana (en px)
@@ -13,7 +13,16 @@ FPS = 30 # Número de fotogramas por segundo
 # Actores
 fondo = Actor("background")
 personaje = Actor("alien", (50, 240))
+caja = Actor("box", (WIDTH - 50, 240))
 
 def draw():
     fondo.draw()
     personaje.draw()
+    caja.draw()
+
+def update(dt):
+    personaje.x += 5 # mover el personaje 5 px a la derecha en cada frame
+    caja.x -= 5 # mover la caja 5 px a la izquierda en cada frame
+    
+    personaje.angle -= 5
+    caja.angle += 5
