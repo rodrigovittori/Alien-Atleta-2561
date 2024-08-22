fce6cb7a63e876bcf9f9f6e6c78fb4a790cdf2bd
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-08-22 16:52:08 -0300

    M5.L3 - Actividad #4: "Función animate()"
    Demostramos los distintos tipos de animación

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 #pgzero
 
 """
-# M5.L3 - Actividad #3: "Controles"
-# Objetivo: Nuestro código anterior ya resuelve el ejercicio, así que sólo desharemos los cambios de assets
+# M5.L3 - Actividad #4: "Función animate()"
+# Objetivo: Demostrar los distintos tipos de animación
 
+Nota: Nuevamente hay cambio de assets - NO compartir código y sólo explicar y exponer
+
+Cambios:
+1º Actor fondo imagen de "background" a "bg"
+2º Desactivar caja: (creación, draw y update)
+----
+3º Agregar varible que controla la animación
+4º Agregar función de animaciones
+5º Presentar función on_key_down()
 """
+
 WIDTH = 600 # Ancho de la ventana (en px)
 HEIGHT = 300 # Alto de la ventana (en px)
 
@@ -12,14 +22,26 @@ TITLE = "Juego del Alien Atleta y sus piruetas" # Título para la ventana del ju
 FPS = 30 # Número de fotogramas por segundo
 
 # Actores
-fondo = Actor("background")
+fondo = Actor("bg")
 personaje = Actor("alien", (50, 240))
-caja = Actor("box", (WIDTH - 50, 240))
+#caja = Actor("box", (WIDTH - 50, 240))
+
+anim = 1
+
+def animar(op):
+    if op == 1:
+        animate(personaje, tween="linear", duration=2, x = WIDTH-35, y = HEIGHT-45)
+    elif (op == 2):
+        animate(personaje, tween="bounce_start_end", duration=2, x = 35, y = 45)
+    elif (op == 3):
+        animate(personaje, tween="accel_decel", duration = 2, x= WIDTH - 35, y = HEIGHT-45)
+    else:
+        animate(personaje, tween="bounce_start_end", duration = 2, x= 35, y = HEIGHT-45)
 
 def draw():
     fondo.draw()
     personaje.draw()
-    caja.draw()
+    #caja.draw()
 
 def update(dt): # Podemos traducir "update" como "actualizar", es decir, en ella contendremos el código que produzca cambios en nuestro juego
 
@@ -29,7 +51,7 @@ def update(dt): # Podemos traducir "update" como "actualizar", es decir, en ella
     elif (keyboard.left or keyboard.a) and (personaje.x > (int(personaje.width/2))):
         personaje.x -= 5
     
-    
+    """
     if (caja.x < (int(caja.width/2))):
         caja.x = WIDTH
     else:
@@ -38,3 +60,15 @@ def update(dt): # Podemos traducir "update" como "actualizar", es decir, en ella
     if caja.angle > 360:
         caja.angle -= 360
     caja.angle += 5
+    """
+
+def on_key_down(key):
+    
+    global anim
+    
+    if (keyboard.space):
+        animar(anim)
+        anim += 1
+        
+        if anim >= 5:
+            anim = 1
