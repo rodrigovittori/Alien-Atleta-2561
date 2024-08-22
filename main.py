63bdad0a08e9269fead1df28a78a993557198e1a
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2024-08-22 16:37:20 -0300

    M5.L2 - Actividad #2: "Claves"
    Agregamos lógica para controlar a nuestro personaje mediante el teclado

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #pgzero
 
 """
-# M5.L2 - Actividad #1: "Actualizando la caja"
-# Objetivo: lograr que los obstáculos reaparezcan tras abandonar la ventana
+# M5.L2 - Actividad #2: "Claves"
+# Objetivo: controlar a nuestro personaje mediante el teclado
 
-1º Agregar condicion (if)
+# NOTA: Modificamos actores por cambio de assets en la tarea
+1º Modificamos fondo de "background" a "bg"
+2º Comentamos la declaración del Actor "caja"
+3º Comentamos la llamada al método draw de la caja (en la fn draw)
+4º Comentamos el código que controla (en la fn update) la caja
+
 """
 WIDTH = 600 # Ancho de la ventana (en px)
 HEIGHT = 300 # Alto de la ventana (en px)
@@ -13,22 +18,24 @@ TITLE = "Juego del Alien Atleta y sus piruetas" # Título para la ventana del ju
 FPS = 30 # Número de fotogramas por segundo
 
 # Actores
-fondo = Actor("background")
+fondo = Actor("bg")
 personaje = Actor("alien", (50, 240))
-caja = Actor("box", (WIDTH - 50, 240))
+#caja = Actor("box", (WIDTH - 50, 240))
 
 def draw():
     fondo.draw()
     personaje.draw()
-    caja.draw()
+    #caja.draw()
 
 def update(dt): # Podemos traducir "update" como "actualizar", es decir, en ella contendremos el código que produzca cambios en nuestro juego
 
-    if (personaje.x > (WIDTH - int(personaje.width/2)) ):
-        personaje.x = 0
-    else:
-        personaje.x +=5 # mover el personaje 5 px a la derecha en cada frame
+    if (keyboard.right or keyboard.d) and (personaje.x < (WIDTH - int(personaje.width/2)) ):
+        personaje.x += 5
+
+    elif (keyboard.left or keyboard.a) and (personaje.x > (int(personaje.width/2))):
+        personaje.x -= 5
     
+    """
     if (caja.x < (int(caja.width/2))):
         caja.x = WIDTH
     else:
@@ -37,3 +44,4 @@ def update(dt): # Podemos traducir "update" como "actualizar", es decir, en ella
     if caja.angle > 360:
         caja.angle -= 360
     caja.angle += 5
+    """
